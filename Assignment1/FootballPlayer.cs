1b6b86a5bdb8d2acbbdbafb77cfc34b954b59964
theashanti <ashanticostamartins@gmail.com>
2022-10-05 11:55:50 +0200

    Add project files.

---
+++ Assignment1/FootballPlayer.cs
@@ -0,0 +1,39 @@
+﻿namespace Assignment1
+{
+    public class FootballPlayer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int ShirtNumber { get; set; }
+
+        public override string ToString()
+        {
+            return Id + ", " + Name + ", Age: " + Age + ", Number: " + ShirtNumber + ".";
+        }
+
+        public void ValidateName()
+        {
+            if (Name.Length < 2) throw new ArgumentException("Name should at least have 2 characters");
+            if (Name == null) throw new NullReferenceException("Name can't be null");
+        }
+
+        public void ValidateAge()
+        {
+            if (Age <= 1) throw new ArgumentOutOfRangeException("Age should be greater than 1");
+        }
+
+        public void ValidateShirtNumber()
+        {
+            if (ShirtNumber > 99 || ShirtNumber < 1) throw new ArgumentOutOfRangeException("Shirt Number must be between 1 and 99");
+        }
+
+        public void Validate()
+        {
+            ValidateName();
+            ValidateAge();
+            ValidateShirtNumber();
+        }
+
+    }
+}
---
+++ Assignment1Tests/FootballPlayerTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assignment1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Assignment1.Tests
+{
+    [TestClass()]
+    public class FootballPlayerTests
+    {
+        public FootballPlayer Correct = new FootballPlayer { Id = 1, Name = "John", Age = 23, ShirtNumber = 25 };
+        public FootballPlayer ShortName = new FootballPlayer { Id = 1, Name = "J", Age = 23, ShirtNumber = 2 };
+        public FootballPlayer NoName = new FootballPlayer { Id = 1, Name = null, Age = 23, ShirtNumber = 5 };
+        public FootballPlayer TooYoung = new FootballPlayer { Id = 1, Name = "John", Age = 0, ShirtNumber = 18 };
+        public FootballPlayer BigShirtNumber = new FootballPlayer { Id = 1, Name = "John", Age = 23, ShirtNumber = 103 };
+
+        [TestMethod()]
+        public void ValidateToString()
+        {
+            Assert.AreEqual(Correct.ToString(), "1, John, Age: 23, Number: 25.");
+
+        }
+
+        [TestMethod()]
+        public void ValidateNameTest()
+        {
+            Correct.ValidateName();
+            Assert.ThrowsException <ArgumentException> (() => ShortName.ValidateName());
+            Assert.ThrowsException<NullReferenceException>(()=> NoName.ValidateName());
+
+
+        }
+
+        [TestMethod()]
+        public void ValidateAgeTest()
+        {
+            Correct.ValidateAge();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => TooYoung.ValidateAge());
+        }
+
+        [TestMethod()]
+        public void ValidateShirtNumberTest()
+        {
+            Correct.ValidateShirtNumber();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(()=> BigShirtNumber.ValidateShirtNumber());
+        }
+    }
+}
